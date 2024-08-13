9916aafe4691efa8594b89e48dc95730d47c8e2c
splexas <official.fluk3@gmail.com>
2024-08-13 15:37:59 +0300

    a

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -16,6 +16,12 @@ __attribute__((noreturn)) void kmain(u32 mb2_boot, u32 mb2_magic)
         hlt();
         goto terminate;
     }
+
+    // Lower memory starts at address 0, and upper memory starts at address 1 megabyte
+
+    u32 min_mem_addr = 0;
+    u32 max_mem_addr = 1024 * 1024;
+
     struct multiboot_tag *tag;
     for (tag = (struct multiboot_tag *) (mb2_boot + 8);
         tag->type != MULTIBOOT_TAG_TYPE_END;
@@ -24,7 +30,8 @@ __attribute__((noreturn)) void kmain(u32 mb2_boot, u32 mb2_magic)
         switch (tag->type) {
             case MULTIBOOT_TAG_TYPE_BASIC_MEMINFO: {
                 struct multiboot_tag_basic_meminfo *mem = (struct multiboot_tag_basic_meminfo *)tag;
-                kprintf("Mem lower: 0x%x\nMem higher: 0x%x\n", mem->mem_lower, mem->mem_upper);
+                kprintf("Mem lower: 0x%x KB\nMem higher: 0x%x KB\n", mem->mem_lower, mem->mem_upper);
+                max_mem_addr += mem->mem_upper * 1024 - 1024 * 1024; // -1 megabyte memory hole
                 break;
             }
             case MULTIBOOT_TAG_TYPE_BOOT_LOADER_NAME: {
@@ -34,6 +41,11 @@ __attribute__((noreturn)) void kmain(u32 mb2_boot, u32 mb2_magic)
             }
         }
     }
+
+    u32 total_memory = max_mem_addr - min_mem_addr;
+    kprintf("Total RAM: %d bytes\n", total_memory);
+
+    // conventional memory is below 1 mb
 
 terminate:
     for (;;);
