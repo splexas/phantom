92b44f147591aed944d13ce487637c1aa8019aca
splexas <official.fluk3@gmail.com>
2024-10-19 23:56:30 +0300

    feat: seperated functions for clarification

--- kernel/src/video/vga.c
+++ kernel/src/video/vga.c
@@ -1,3 +1,4 @@
+#include <io.h>
 #include <stdint.h>
 #include <video/vga.h>
 
@@ -15,10 +16,22 @@ static inline uint16_t vga_entry(unsigned char c, enum vga_color color)
     return (uint16_t)c | (uint16_t)color << 8;
 }
 
-void vga_init()
+static void vga_disable_cursor()
+{
+    outb(0x3D4, 0x0A);
+    outb(0x3D5, 0x20);
+}
+
+static void vga_clear()
 {
     for (uint32_t i = 0; i < VGA_WIDTH * VGA_HEIGHT; i++)
         vga_buf[i] = vga_entry(' ', VGA_COLOR_WHITE);
+}
+
+void vga_init()
+{
+    vga_disable_cursor();
+    vga_clear();
 }
 
 void vga_putchar(char c)
