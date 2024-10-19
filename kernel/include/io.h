d1f24a617446c850473ffecfece5e63ec5834c29
splexas <official.fluk3@gmail.com>
2024-10-19 23:54:15 +0300

    feat: IO functions (inb, outb)

---
+++ kernel/include/io.h
@@ -0,0 +1,18 @@
+#ifndef _IO_H
+#define _IO_H
+
+#include <stdint.h>
+
+inline uint8_t inb(uint16_t port)
+{
+    uint8_t ret;
+    asm volatile("inb %w1, %b0" : "=a"(ret) : "Nd"(port) : "memory");
+    return ret;
+}
+
+inline void outb(uint16_t port, uint8_t val)
+{
+    asm volatile("outb %b0, %w1" : : "a"(val), "Nd"(port) : "memory");
+}
+
+#endif
