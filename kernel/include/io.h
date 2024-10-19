987ef7bc525c836607e680f18c30a7f321e0fd93
splexas <official.fluk3@gmail.com>
2024-10-20 02:00:13 +0300

    fix: removed incline functions

--- kernel/include/io.h
+++ kernel/include/io.h
@@ -3,14 +3,14 @@
 
 #include <stdint.h>
 
-inline uint8_t inb(uint16_t port)
+uint8_t inb(uint16_t port)
 {
     uint8_t ret;
     asm volatile("inb %w1, %b0" : "=a"(ret) : "Nd"(port) : "memory");
     return ret;
 }
 
-inline void outb(uint16_t port, uint8_t val)
+void outb(uint16_t port, uint8_t val)
 {
     asm volatile("outb %b0, %w1" : : "a"(val), "Nd"(port) : "memory");
 }
