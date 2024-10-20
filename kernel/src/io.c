0a711a5505b94ee43336bb883eadfd48beec5bed
splexas <official.fluk3@gmail.com>
2024-10-20 16:01:51 +0300

    refactor: made io functions more usable in codebase

---
+++ kernel/src/io.c
@@ -0,0 +1,14 @@
+#include <io.h>
+
+uint8_t inb(uint16_t port)
+{
+    uint8_t ret;
+    asm volatile("inb %w1, %b0" : "=a"(ret) : "Nd"(port) : "memory");
+    return ret;
+}
+
+void outb(uint16_t port, uint8_t val)
+{
+    asm volatile("outb %b0, %w1" : : "a"(val), "Nd"(port) : "memory");
+}
+
